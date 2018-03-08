bfbd2af2bad77843434b873c93f073035732d122
kukukoko11 <kukukoko11@gmail.com>
2018-03-07 18:01:58 -0600

    Initial application generated by JHipster-4.14.1

---
+++ src/main/webapp/app/blocks/config/localstorage.config.js
@@ -0,0 +1,14 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('myapptestApp')
+        .config(localStorageConfig);
+
+    localStorageConfig.$inject = ['$localStorageProvider', '$sessionStorageProvider'];
+
+    function localStorageConfig($localStorageProvider, $sessionStorageProvider) {
+        $localStorageProvider.setKeyPrefix('jhi-');
+        $sessionStorageProvider.setKeyPrefix('jhi-');
+    }
+})();
---
+++ src/main/webapp/app/components/form/pagination.constants.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('myapptestApp')
+        .constant('paginationConstants', {
+            'itemsPerPage': 20
+        });
+})();
